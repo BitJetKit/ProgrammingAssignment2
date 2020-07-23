7483fe27be2543b18cfd825c88e1551280326542
Bit Jet Kit <jkelly80@jh.edu>
2020-07-23 12:14:45 -0500

    Set i, set, x, and i again for the first part of makeCacheMatrix.

--- PA2.R
+++ PA2.R
@@ -1,3 +1,9 @@
 ## This function, makeCacheMatrix, makes a specific matrix object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {}
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+}
